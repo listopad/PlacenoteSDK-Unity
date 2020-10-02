bf01fda7d7f30401ea76159779f7e0e67a991dea
Artem Ryder <de.sign@my.com>
2020-10-02 08:46:08 +0300

    Init

---
+++ Assets/SceneHandler.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SceneHandler : MonoBehaviour
+{
+    public void RunScene(int index)
+    {
+        SceneManager.LoadScene(index);
+    }
+}
